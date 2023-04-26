de4a4b51bbcd8092c02177fd709a8a4b2172a1cb
Fatma <fmutlu3002@gmail.com>
2023-04-26 11:01:32 +0300

    fix

--- Presentation/FLoraAPI.API/Controllers/UsersController.cs
+++ Presentation/FLoraAPI.API/Controllers/UsersController.cs
@@ -13,9 +13,7 @@ namespace FLoraAPI.API.Controllers
     public class UsersController : ControllerBase
     {
         readonly IMediator _mediator;
-        #region yorum
-        //bize bu türde bir request gelmiş, ama nasıl gelmiş body route ?body olmadığı kesin demek istiyorum ama yapılacak işlemi bildiğim için body cevPP YANİ body tamam ama json ddin ben ortada jsonlık bir durum görmüyorum  tamam anlaştık balım ben bir su geliyooommmya gittim aslında yatak almaya gittiğimde de onu da söylemeyeyim yani dedim iyi anlaşıldıok, doki balımmmmmmmmmmmmmmmm teşşekkürler canım ben daha çok seviyorums izi<3 sensin ellerinize sağlık derim girmeyelim hani  yok bildiğim yok yok tmamdır sen atıver bana <3 şu an olmaaaaz aleyna bana dönük tamam <3 hazırımmmmmmmm tamam
-        #endregion
+       
         readonly IConfiguration _configuration;
         public UsersController(IMediator mediator, IConfiguration configuration)
         {
